5c3263e060245250df978406492c3a67e7295e6e
fabio-f6 <fabio.fbff@icloud.com>
2026-02-18 13:48:24 +0000

    Question model update
    And Admin panel updated

--- hitop2/polls/models.py
+++ hitop2/polls/models.py
@@ -1,10 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
-	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=255)
 
-class Choice(models.Model):
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_text = models.CharField(max_length=20)
-	votes = models.IntegerField(default=0)
+    ANSWER_CHOICES = [
+        ('1', 'Nunca'),
+        ('2', 'Raramente'),
+        ('3', 'Às vezes'),
+        ('4', 'Sempre'),
+    ]
+
+    def __str__(self):
+        return self.question_text
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=1, choices=Question.ANSWER_CHOICES)
+    answered_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.question.question_text}: {self.get_answer_display()}"
