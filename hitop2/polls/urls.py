3a3a86d98a30c734c10a511e38432db35e8cd545
fabio-f6 <fabio.fbff@icloud.com>
2026-02-18 17:46:59 +0000

    Questionnaire page created

--- hitop2/polls/urls.py
+++ hitop2/polls/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
-	path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("questionnaire/", views.questionnaire, name="questionnaire"),
+	path("thank-you/", views.thank_you, name="thank_you"),
 ]
--- hitop2/polls/views.py
+++ hitop2/polls/views.py
@@ -1,4 +1,42 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Question, UserAnswer
+
+# Mantemos as opções para o template (mesmo que estejam no model)
+answer_choices = Question.ANSWER_CHOICES
+
+@login_required
+def questionnaire(request):
+    questions = Question.objects.all()
+
+    if request.method == "POST":
+        for question in questions:
+            field_name = f"question_{question.id}"
+            selected_value = request.POST.get(field_name)
+
+            if selected_value:
+                # Salva ou atualiza a resposta do usuário
+                UserAnswer.objects.update_or_create(
+                    user=request.user,
+                    question=question,
+                    defaults={'answer': selected_value}
+                )
+
+        # redireciona para uma página de "Obrigado" ou resultados
+        return redirect("polls:thank_you")
+
+    return render(request, "polls/questionnaire.html", {
+        "questions": questions,
+        "answer_choices": answer_choices
+    })
 
 def index(request):
-	return HttpResponse("Hello, world. You are at the poll index.")
+    latest_question_list = Question.objects.order_by("-id")
+    context = {
+    	'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+@login_required
+def thank_you(request):
+    return render(request, "polls/thank_you.html")
